6848cde58217cc94cf0be74777541a7f5eb1d803
agarwalsiddhant10 <agarwalsiddhant10@gmail.com>
2018-05-04 14:20:22 +0530

    Changed merged cluster.cpp

--- HOG_Doc/merge_cluster.cpp
+++ HOG_Doc/merge_cluster.cpp
@@ -1,9 +1,9 @@
 #define D_THRESH 5
 
-vector<vector<Point> > merge_cluster(vector<vector<Point> > Clusters,int no_clusters,int no_points) 
+vector<cluster> merge_cluster(vector<cluster > Clusters,int no_clusters,int no_points) 
 {
 	int count;
-	vector<vector<Point> > Final;
+	vector<cluster> Final;
 	Final.clear();
 	Final.resize(no_clusters);
 	for(int k=0;k<no_clusters;k++)
@@ -12,9 +12,9 @@ vector<vector<Point> > merge_cluster(vector<vector<Point> > Clusters,int no_clus
 		{
 			double d_min=FLT_MIN;
 
-			for(auto it=Clusters[k].begin();it!=Clusters[k].end();it++)
+			for(auto it=Clusters[k].members.begin();it!=Clusters[k].members.end();it++)
 			{
-				for(auto it1=Clusters[k1].begin();it1!=Clusters[k1].end();it1++)
+				for(auto it1=Clusters[k1].members.begin();it1!=Clusters[k1].members.end();it1++)
 				{
 					if(distance(*it1,*it)<d_min)
 						d_min=distance(*it1,*it);
@@ -22,14 +22,25 @@ vector<vector<Point> > merge_cluster(vector<vector<Point> > Clusters,int no_clus
 			}
 			if(d_min<D_THRESH)
 			{
-				Final[count]=Cluster[k];
-				Final[count].insert(Final[count].end(),Cluster[k1].begin(),Cluster[k1].end());
+				Final[count].members=Cluster[k].members;
+				Final[count].insert(Final[count].members.end(),Cluster[k1].members.begin(),Cluster[k1].members.end());
+				int n1=Cluster[k].members.size();
+				int n2=Cluster[k1].members.size();
+				Point p1=Cluster[k].mean;
+				Point p2=Cluster[k1].mean;
+				Final[count].mean.x=(n1*p1.x+n2*p2.x)/(n1+n2);
+				Final[count].mean.y=(n1*p1.y+n2*p2.y)/(n1+n2);
 				count++;
 			}
 			else
 			{
-				Final[count++]=Cluster[k];
-				Final[count++]=Cluster[k1];
+				Final[count].members=Cluster[k].members;
+				Final[count].mean=Cluster[k].mean;
+				count++;
+				Final[count].members=Cluster[k1].members;
+				Final[count].mean=Cluster[k1].mean;
+				count++;
+
 			}
 
 		}
@@ -38,5 +49,3 @@ vector<vector<Point> > merge_cluster(vector<vector<Point> > Clusters,int no_clus
 	return Final;
 
 }
-
-//Call shift_center after this
